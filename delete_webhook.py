7a8ec2153e80b5dc334ffabf2605782333ae4de7
Kf637 <43268197+Kf637@users.noreply.github.com>
2023-02-27 09:50:45 +0100

    Version 0.05
    Removed useless lines

--- delete_webhook.py
+++ delete_webhook.py
@@ -47,12 +47,7 @@ while True:
         continue
 
     response = requests.delete(url)
-    if response.status_code == requests.codes.not_found:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print('Webhook not found')
-        url = None
-    elif response.status_code == requests.codes.no_content:
-        time.sleep(.4)
+    if response.status_code == requests.codes.no_content:
         os.system('cls' if os.name == 'nt' else 'clear')
         print(f'Webhook deleted successfully\n\nDeleted URL: {url}\nWebhook Name: {webhook_info["name"]}\nToken: {webhook_info["token"]}\nGuild ID: {webhook_info["guild_id"]}\nChannel ID: {webhook_info["channel_id"]}\nWebhook ID: {webhook_info["id"]}\nApplication ID: {webhook_info["application_id"]}\nAvatar: {webhook_info["avatar"]}\nWebhook Type: {webhook_info["type"]}\nApplication ID: {webhook_info["application_id"]}')
         url = None
